498538d1ee4a52ed289383cbf8df7f0e7e417f3a
jptrindade3 <jpr.trindade@gmail.com>
2020-05-24 16:45:27 -0300

    Front da página countdown training criado
    Além do front igual ao da identidade visual, a função de contagem regressiva no relógio também está funcionando.

--- livel/src/routes.js
+++ livel/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import TimeSchedule from './Components/Pages/Coachee/TimeSchedule'
 import TrainingTypes from './Components/Pages/Coachee/TrainingTypes'
+import CountdownTraining from './Components/Pages/Coachee/CountdownTraining';
 
 export default function Routes(){
     return (
@@ -16,7 +17,7 @@ export default function Routes(){
                 <Route path="/trainingTypes" component={TrainingTypes} />
                 {/* <Route path="/midTraining" component={MidTraining} /> */}
                 <Route path="/timeSchedule" component={TimeSchedule} />
-                {/* <Route path="/countdownTraining" component={CountdownTraning} /> */}
+                <Route path="/countdownTraining" component={CountdownTraining} /> 
                 {/* <Route path="/profile" component={Profile} /> */}
                 {/* <Route path="/rewards" component={Rewards} /> */}
                 {/* <Route path="/body" component={Body} /> */}
