f0d1221c8fb177f5e6364e6d8a2f55dd4a2929f5
Carolina <carolinaalvares@cpejr.com.br>
2020-05-24 22:59:24 -0300

    SignUp finished
    Deu problema na hora de passar pro chrome. Não atualizava as mudanças no css.

--- livel/src/Components/Pages/Login/Signup/Signup.js
+++ livel/src/Components/Pages/Login/Signup/Signup.js
@@ -9,41 +9,58 @@ import './SignUp.css'
 
 
 export default function SignUp(){
-    return(
-        <div className="Container">
-            <PurpleBody/>
-            <div className="SignUpForm">
-                <form> 
-                {/* onSubmit={handleSignUp} */}
-                        <input
-                                placeholder="Nome"
-                                // value={name}
-                                // onChange={e => setName(e.target.value)}
-                        />
-                        <input 
-                                placeholder="Sobrenome" 
-                                // value={lastName}
-                                // onChange={e => setLastName(e.target.value)}
-                        />
-
-                        <input
-                                placeholder="CPF" 
-                                // value={cpf}
-                                // onChange={e => setCpf(e.target.value)}
-                        />
-
-                        <input
-                                type="email" 
-                                placeholder="E-mail" 
-                                // value={email}
-                                // onChange={e => setEmail(e.target.value)}
-                        />
-                </form>
-
-                <Link type="submit" to="/">
-                    <div className="buttonEnviar"><a>Enviar</a></div>
-                </Link>
-                
-            </div>
-        </div>
-    )}
+        return(
+                <div className="purpleBodyContainer">
+                        <div className="Container">
+                        <PurpleBody/>
+                            <form> 
+                            {/* onSubmit={handleRequestNumber} */}
+                                        <input
+                                            type="text"
+                                            placeholder="Nome"
+                                            name="Nome" 
+                                            required="true"
+                                            // value={name}
+                                            // onChange={e => setName(e.target.value)}
+                                         />
+                                         <input 
+                                            type="text"
+                                            placeholder="Sobrenome" 
+                                            name="Sobrenome" 
+                                            required="true"
+                                            // value={lastName}
+                                            // onChange={e => setLastName(e.target.value)}
+                                        />
+                                        <input
+                                            type="email" 
+                                            placeholder="E-mail" 
+                                            name="email" 
+                                            required="true"
+                                            // value={email}
+                                            // onChange={e => setEmail(e.target.value)}
+                                        />
+                                        <input
+                                            type="text"
+                                            placeholder="Criar senha" 
+                                            name="Senha" 
+                                            required="true"
+                                            // value={cpf}
+                                            // onChange={e => setCpf(e.target.value)}
+                                         />
+                                        <input
+                                            type="text"
+                                            placeholder="Confirmar senha" 
+                                            name="ConfirmarSenha" 
+                                            required="true"
+                                            // value={cpf}
+                                            // onChange={e => setCpf(e.target.value)}
+                                         />
+                            </form>
+                            <Link type="submit" to="/">
+                                <div className="buttonCadastrar"><h1>CADASTRAR</h1></div>
+                            </Link>
+                        </div>
+                        <div className="title"><h1>Realize seu cadastro </h1></div>
+                </div>
+            )
+    }
