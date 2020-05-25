cf9ce8f69a66dd19a92dcd5f9d9e575c5de9afa0
Arthur <arthurdeolima@gmail.com>
2020-05-25 16:47:46 -0300

    /body concluido

--- livel/src/Components/Header/Header.js
+++ livel/src/Components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
+
+import { IconContext } from "react-icons";
+import { IoIosArrowBack } from "react-icons/io";
 
 import './Header.css'
 
 export default function Header(props){
+    let history = useHistory()
+
     return (
         <div className="HeaderContainer">
 
@@ -10,26 +16,28 @@ export default function Header(props){
 
             <div className='headerContent'>
                 {(props.img!=null) &&
-                    <img src={props.img} alt="imagem" className="imagemHeader" />
+                    <img src={props.img} alt="imagem" className="imagemHeader" onClick={()=>history.push('/profile')}/>
                 }
 
                 {(props.name!=null) &&
-                    <div className="name">{props.name}</div>
+                    <div className="name" onClick={()=>history.push('/profile')}>{props.name}</div>
                 }
 
                 {(props.level!=null) &&
-                    <img src={props.level} alt="level"/>
+                    <img src={`/images/level/nivel${props.level}.png`} alt="level" className='level' />
                 }
 
                 {(props.icons!=null) &&
                     <div>
-                        <img src='/images/icon1.png' alt="icone1" style={{marginRight: '16px'}} />
-                        <img src='/images/icon2.png' alt="icone1"/>
+                        <img src='/images/bodyIcon.png' alt="icone1" style={{marginRight: '16px'}} onClick={()=>history.push('/body')}/>
+                        <img src='/images/rewardsIcon.png' alt="icone1" onClick={()=>history.push('/rewards')}/>
                     </div>
                 }
 
                 {(props.back!=null) &&
-                    <div></div>
+                    <IconContext.Provider value={{ size: '3em', color: "#632467" }}>
+                        <IoIosArrowBack  onClick={()=>history.push('/trainingTypes')}/>
+                    </IconContext.Provider>
                 }
 
             </div>
--- livel/src/Components/Pages/Coachee/Body/Elements/InterativeBody.js
+++ livel/src/Components/Pages/Coachee/Body/Elements/InterativeBody.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 
 function partStyle(style_top, style_left){
     return {
@@ -9,101 +9,124 @@ function partStyle(style_top, style_left){
     }
 }
 
-export default function InterativeBody(){
+export default function InterativeBody(props){
 
     const [reload, setReload] = useState(true)
 
     const [parts, setParts] = useState([
         {
+            displayName: 'CORAÇÃO',
+            part_name: 'coracao',
+            top: '24%',
+            left: '61%',
+            selected: false
+        },
+        {
+            displayName: 'CABEÇA',
             part_name: 'cabeca',
             top: '4%',
             left: '50%',
             selected: false
         },
         {
+            displayName: 'PESCOÇO',
             part_name: 'pescoco',
             top: '13%',
             left: '50%',
             selected: false
         },
         {
+            displayName: 'COLUNA',
             part_name: 'coluna',
             top: '33%',
             left: '50%',
             selected: false
         },
         {
+            displayName: 'COTOVELO DIREITO', 
             part_name: 'cotovelo_direito',
             top: '35%',
             left: '13%',
             selected: false
         },
         {
+            displayName: 'COTOVELO ESQUERDO',
             part_name: 'cotovelo_esquerdo',
             top: '35%',
             left: '88%',
             selected: false
         },
         {
+            displayName: 'JOELHO DIREITO',
             part_name: 'joelho_direito',
             top: '71%',
             left: '33%',
             selected: false
         },
         {
+            displayName: 'JOELHO ESQUERDO',
             part_name: 'joelho_esquerdo',
             top: '71%',
             left: '66%',
             selected: false
         },
         {
+            displayName: 'MÃO DIREITA',
             part_name: 'mao_direita',
             top: '54%',
             left: '6%',
             selected: false
         },
         {
+            displayName: 'MÃO ESQUERDA',
             part_name: 'mao_esquerda',
             top: '54%',
             left: '95%',
             selected: false
         },
         {
+            displayName: 'OMBRO DIREITO',
             part_name: 'ombro_direito',
             top: '20%',
             left: '24%',
             selected: false
         },
         {
+            displayName: 'OMBRO ESQUERDO',
             part_name: 'ombro_esquerdo',
             top: '20%',
             left: '77%',
             selected: false
         },
         {
+            displayName: 'PUNHO DIREITO',
             part_name: 'punho_direito',
             top: '48%',
             left: '6%',
             selected: false
         },
         {
+            displayName: 'PUNHO ESQUERDO',
             part_name: 'punho_esquerdo',
             top: '48%',
             left: '95%'
         },
         {
+            displayName: 'QUADRIL',
             part_name: 'quadril',
             top: '51%',
             left: '50%',
             selected: false
         },
         {
+            displayName: 'TORNOZELO DIREITO',
             part_name: 'tornozelo_direito',
             top: '91%',
             left: '35%',
             selected: false
         },
         {
+            displayName: 'TORNOZELO ESQUERDO',
             part_name: 'tornozelo_esquerdo',
             top: '91%',
             left: '65%',
@@ -116,6 +139,7 @@ export default function InterativeBody(){
         tempParts[index].selected = !tempParts[index].selected
         setParts(tempParts)
         setReload(!reload)
+        props.handleSelect(parts[index].displayName)
     }
     
     return (
