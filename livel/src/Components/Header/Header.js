58b2ca5d4da33be9dcc928791e4a10c9104f67a2
Arthur <arthurdeolima@gmail.com>
2020-05-26 15:56:58 -0300

    Header Atualizado

--- livel/src/Components/Header/Header.js
+++ livel/src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Link, useHistory} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 
 import { IconContext } from "react-icons";
 import { IoIosArrowBack } from "react-icons/io";
@@ -31,10 +31,8 @@ export default function Header(props){
 
                 {(props.icons!=null) &&
                     <div>
-                        <Link className="button"  type="button" to="/rewards">
-                        <img src='/images/icon1.png' alt="icone1" style={{marginRight: '16px'}} />
-                        </Link>
-                        {/* <img src='/images/icon2.png' alt="icone1"> */}
+                        <img src='/images/bodyIcon.png' alt="icone1" style={{marginRight: '16px'}} onClick={()=>history.push('/body')}/>
+                        <img src='/images/rewardsIcon.png' alt="icone1" onClick={()=>history.push('/rewards')}/>
                     </div>
                 }
 
