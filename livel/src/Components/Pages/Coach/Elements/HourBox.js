57b8ca60ea17ce9a582ef59d1a4fc142719f7a20
Carolina <carolinaalvares@cpejr.com.br>
2020-06-21 23:26:49 -0300

    mudanca de aproach

--- livel/src/Components/Pages/Coach/Elements/HourBox.js
+++ livel/src/Components/Pages/Coach/Elements/HourBox.js
@@ -5,44 +5,15 @@ import ReactDOM from 'react-dom';
 const render = ReactDOM.render;
 
 
-class CompatibleHours extends Component{
-    render(){
-        let AVAILABLE_TIME = ['13:00', '14:00', '14:30'];
-        let DAY_HOURS = ['13:00', '13:30', '14:00', '14:30', '15:00', '15:30'];
-        let compatible = [];
-        let incompatible = [];
-        do{
-        for(let i=0; i<20; i++){
-            var num = AVAILABLE_TIME[i];
-                for(let j=0; j<20; j++){
-                    if(num==DAY_HOURS[j]){
-                        for(let k=0; k<20; k++){
-                            if (num !=compatible[k]){
-                                var compatibleNumber = num;
-                            }
-                            compatible.push(compatibleNumber);
-                        }
-                    }
-                    else{
-                        for(let k=0; k<20; k++){
-                            if (num !=compatible[k]){
-                                if(num!=incompatible){
-                                    var incompatibleNumber = num; 
-                                    incompatible.push(incompatibleNumber);
-                                }
-                            }
+let AVAILABLE_TIME = {
+    '13:00' : 'true',
+    '13:30' : 'false',
+    '14:00' : 'true',
+    '14:30' : 'true',
+    '15:00' : 'false',
+}
 
-                            else {
-                            }
-                        }
-                    }
-                }
-      }
-    }
-    while(i=<AVAILABLE_TIME.length);
-      return incompatible;
-    }
-  }
+
 
   export default function HourBox(props){
     return (
@@ -66,6 +37,17 @@ class CompatibleHours extends Component{
 
 
 
+// function CompatibleHours (props){
+//     return{
+//         for(let i=0; i<20; i++){
+//             var num = AVAILABLE_TIME[i];
+//                 incompatible.pop(num);
+//         }
+//         for(let j=0; j<20; j++){
+//             return incompatible[j];  
+//         }
+//     }
+// }
 
 
 // export default function HourBox(props){
@@ -81,3 +63,46 @@ class CompatibleHours extends Component{
 //         </div>
 //     )
 // }   
+
+// class CompatibleHours extends Component{
+//     render(){
+//         let AVAILABLE_TIME = ['13:00', '14:00', '14:30'];
+//         let DAY_HOURS = ['13:00', '13:30', '14:00', '14:30', '15:00', '15:30'];
+//         let compatible = [];
+//         let incompatible = [];
+//         do{
+//         for(let i=0; i<20; i++){
+//             var num = AVAILABLE_TIME[i];
+//                 for(let j=0; j<20; j++){
+//                     if(num==DAY_HOURS[j]){
+//                         for(let k=0; k<20; k++){
+//                             if (num !=compatible[k]){
+//                                 var compatibleNumber = num;
+//                             }
+//                             compatible.push(compatibleNumber);
+//                         }
+//                     }
+//                     else{
+//                         for(let k=0; k<20; k++){
+//                             if (num !=compatible[k]){
+//                                 if(num!=incompatible){
+//                                     var incompatibleNumber = num; 
+//                                     incompatible.push(incompatibleNumber);
+//                                 }
+//                             }
+
+//                             else {
+//                             }
+//                         }
+//                     }
+//                 }
+//       }
+//     }
+//     while(i=<AVAILABLE_TIME.length);
+//       return incompatible;
+//     }
+//   }
+
+// let AVAILABLE_TIME = ['13:00', '14:00', '14:30'];
+// let DAY_HOURS = ['13:00', '13:30', '14:00', '14:30', '15:00', '15:30'];
+// let incompatible =  DAY_HOURS;
