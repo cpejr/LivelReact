ed97be69763f999bd32d9afd38ec55265711908e
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-08-10 18:32:04 -0300

    Checkpoint no ResultadoRegistra

--- livel/src/Components/Pages/Coach/Elements/PopUpResultadoRegistra/PopUp.js
+++ livel/src/Components/Pages/Coach/Elements/PopUpResultadoRegistra/PopUp.js
@@ -6,10 +6,10 @@ import { AiFillCloseCircle } from "react-icons/ai";
 
 import "./PopUpRegisterResult.css";
 
-export default function PopUp({functionState}) {
+export default function PopUp({ functionState }) {
     let metricasAluno = {
-        AlunoID: "tetete",
-        dataResultado: "07/08/2020",
+        AlunoID: "",
+        dataResultado: "",
         Peso: "",
         Altura: "",
         AlongamentoNivel: "",
@@ -23,10 +23,14 @@ export default function PopUp({functionState}) {
         Coxa: "",
     };
 
+    function funcao_enviar() {
+        console.log(metricasAluno);
+    }
     return (
         <div className="Modal">
             <div className="ContainerModal">
-                <div className="ClosePopUp2"
+                <div
+                    className="ClosePopUp2"
                     onClick={() => functionState(false)}
                 >
                     <AiFillCloseCircle size={35} />
@@ -41,58 +45,147 @@ export default function PopUp({functionState}) {
                             type="text"
                             id="alunoID"
                             name="inp_alunoID"
+                            onChange={(e) =>
+                                (metricasAluno.AlunoID = e.target.value)
+                            }
                         ></input>
                     </div>
                     <div className="div_data">
                         <label>Data:</label>
-                        <input type="text" id="data" name="inp_data" />
+                        <input
+                            type="text"
+                            id="data"
+                            name="inp_data"
+                            onChange={(e) =>
+                                (metricasAluno.dataResultado = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_peso">
                         <label>Peso:</label>
-                        <input type="text" id="peso" name="inp_peso" />
+                        <input
+                            type="text"
+                            id="peso"
+                            name="inp_peso"
+                            onChange={(e) =>
+                                (metricasAluno.Peso = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_altura">
                         <label>Altura:</label>
-                        <input type="text" id="altura" name="inp_altura" />
+                        <input
+                            type="text"
+                            id="altura"
+                            name="inp_altura"
+                            onChange={(e) =>
+                                (metricasAluno.Altura = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_along">
                         <label>Alongam. Nivel:</label>
-                        <input type="text" id="alogamento" name="inp_along" />
+                        <input
+                            type="text"
+                            id="alogamento"
+                            name="inp_along"
+                            onChange={(e) =>
+                                (metricasAluno.AlongamentoNivel = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_abno">
                         <label>Abdominais:</label>
-                        <input type="text" id="Name" name="inp_abno" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_abno"
+                            onChange={(e) =>
+                                (metricasAluno.Abdominais = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_flexo">
                         <label>Flexões:</label>
-                        <input type="text" id="Name" name="inp_flex" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_flex"
+                            onChange={(e) =>
+                                (metricasAluno.Flexoes = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_bar">
                         <label>Barra:</label>
-                        <input type="text" id="Name" name="inp_bar" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_bar"
+                            onChange={(e) =>
+                                (metricasAluno.barra = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_neck">
                         <label>Pescoço:</label>
-                        <input type="text" id="Name" name="inp_neck" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_neck"
+                            onChange={(e) =>
+                                (metricasAluno.Pescoco = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_cintura">
                         <label>Cintura:</label>
-                        <input type="text" id="Name" name="inp_cintura" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_cintura"
+                            onChange={(e) =>
+                                (metricasAluno.Cintura = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_quadril">
                         <label>Quadril:</label>
-                        <input type="text" id="Name" name="inp_quadril" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_quadril"
+                            onChange={(e) =>
+                                (metricasAluno.Quadril = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_biceps">
                         <label>Biceps:</label>
-                        <input type="text" id="Name" name="inp_biceps" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_biceps"
+                            onChange={(e) =>
+                                (metricasAluno.Biceps = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="div_coxa">
                         <label>Coxa:</label>
-                        <input type="text" id="Name" name="inp_coxa" />
+                        <input
+                            type="text"
+                            id="Name"
+                            name="inp_coxa"
+                            onChange={(e) =>
+                                (metricasAluno.Coxa = e.target.value)
+                            }
+                        />
                     </div>
                     <div className="button_submit">
-                        <button className="button">Enviar</button>
+                        <button
+                            onClick={funcao_enviar}
+                        />
                     </div>
                 </form>
             </div>
