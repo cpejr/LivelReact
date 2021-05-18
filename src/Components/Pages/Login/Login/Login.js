ae566a0981e2ad4a48afe7da0491e9ea2c85243a
ArthurLima <arthurdeolima@gmail.com>
2021-05-17 23:05:53 -0300

    Debugging 1

--- src/Components/Pages/Login/Login/Login.js
+++ src/Components/Pages/Login/Login/Login.js
@@ -1,102 +1,121 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import {useHistory} from 'react-router-dom'
-import {alunoLogin} from '../../../../services/backEnd'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { alunoLogin } from "../../../../services/backEnd";
 import { login } from "../../../../services/auth";
 
-import {TextField} from '@material-ui/core';
+import { TextField } from "@material-ui/core";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
-
-import './Login.css'
+import "./Login.css";
 
 const styles = {
-    inputStyle: {
-        borderBottom: '1px solid white',
-        color: "white"
-    },
-    inputLabel: {
+  inputStyle: {
+    borderBottom: "1px solid white",
     color: "white",
-    }
+  },
+  inputLabel: {
+    color: "white",
+  },
 };
 
 // isCoach
 //  true -> Coach
-// false -> Coachee 
+// false -> Coachee
 
-function Login(props){
-    const [matricula, setMatricula] = useState()
-    const [senha, setSenha] = useState()
-    const history = useHistory()
+function Login(props) {
+  const [matricula, setMatricula] = useState();
+  const [senha, setSenha] = useState();
+  const history = useHistory();
 
-    let isCoach = props.location.state.isCoach;
+  let isCoach = props.location.state.isCoach;
 
-    const { classes} = props;
-    // const isCoach = data
+  const { classes } = props;
+  // const isCoach = data
 
-    function handleSubmit(e){
-        e.preventDefault()
-        const request = async () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const request = async () => {
+      alunoLogin(matricula, senha)
+        .then(async (result) => {
+          if (result.LOGIN.ID_Aluno > 0 && result.LOGIN.Status === 1) {
+            await login(result);
 
-                alunoLogin(matricula, senha).then(async (result)=>{
-                    if(result.LOGIN.ID_Aluno > 0 && result.LOGIN.Status === 1){
-                        await login(result)
+            // Validação de coach ou nao
+            isCoach ? history.push("/coach") : history.push("/trainingTypes");
+          } else {
+            alert("Dados incorretos");
+            history.push("/login");
+          }
+        })
+        .catch((error) => {
+          alert(error);
+          history.push("/login");
+        });
+    };
+    request();
+  }
 
-                        // Validação de coach ou nao 
-                        isCoach?
-                            history.push('/coach')
-                            : history.push('/trainingTypes')
-                    }
-                        
-                    else{
-                        alert('Dados incorretos')
-                        history.push('/login')
-                    }
-                }).catch(error=>{
-                    alert(error)
-                    history.push('/login')
-                })
-        }
-        request();
-    }
+  return (
+    <div className="bodyContainer">
+      <img src="/images/Logo_Livel.png" alt="Logo do Livel" className="logo" />
+      {isCoach ? (
+        <h1 className="titleCoachOrCoachee">Olá, Professor</h1>
+      ) : (
+        <h1 className="titleCoachOrCoachee">Olá, Aluno</h1>
+      )}
+      <div className="LoginTitle">Login</div>
+      <div className="inputsContainer">
+        <TextField
+          className="matriculaInput"
+          label="matrícula"
+          onChange={(e) => {
+            setMatricula(e.target.value);
+          }}
+          InputLabelProps={{
+            classes: { root: classes.inputLabel, focussed: classes.inputLabel },
+          }}
+          InputProps={{
+            classes: {
+              root: classes.inputStyle,
+              focussed: classes.inputStyle,
+              notchedOutline: classes.inputStyle,
+            },
+          }}
+        />
 
-    return(
-        <div className="bodyContainer">
-            <img src='/images/Logo_Livel.png' alt="Logo do Livel" className='logo'/>
-            {isCoach ? <h1 className="titleCoachOrCoachee">Olá, Professor</h1> 
-                    : <h1 className="titleCoachOrCoachee">Olá, Aluno</h1> } 
-            <div className='LoginTitle'>Login</div>
-            <div className='inputsContainer'>
-                <TextField  className="matriculaInput" label="matrícula" onChange={(e)=>{setMatricula(e.target.value)}}
-                    InputLabelProps={{
-                        classes: {root: classes.inputLabel, focussed: classes.inputLabel}
-                    }}
-                    InputProps={{
-                        classes: {root: classes.inputStyle, focussed: classes.inputStyle, notchedOutline: classes.inputStyle}
-                    }}
-                />
-    
-                <TextField  className="senhaInput" label="senha" onChange={(e)=>{setSenha(e.target.value)}}
-                    type="password"
-                    InputLabelProps={{
-                        classes: {root: classes.inputLabel, focussed: classes.inputLabel}
-                    }}
-                    InputProps={{
-                        classes: {root: classes.inputStyle, focussed: classes.inputStyle, notchedOutline: classes.inputStyle}
-                    }}
-                />
-            </div>
-            <div className='loginClick' onClick={handleSubmit}>ENTRAR</div>
-            <Link className='link' to='/signUp'>Solicitar número de matrícula</Link>
-        </div>
-    )
+        <TextField
+          className="senhaInput"
+          label="senha"
+          onChange={(e) => {
+            setSenha(e.target.value);
+          }}
+          type="password"
+          InputLabelProps={{
+            classes: { root: classes.inputLabel, focussed: classes.inputLabel },
+          }}
+          InputProps={{
+            classes: {
+              root: classes.inputStyle,
+              focussed: classes.inputStyle,
+              notchedOutline: classes.inputStyle,
+            },
+          }}
+        />
+      </div>
+      <div className="loginClick" onClick={handleSubmit}>
+        ENTRAR
+      </div>
+      <Link className="link" to="/signUp">
+        Solicitar número de matrícula
+      </Link>
+    </div>
+  );
 }
 
-
 Login.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
-
 
 export default withStyles(styles)(Login);
--- src/services/backEnd.js
+++ src/services/backEnd.js
@@ -1,204 +1,246 @@
-import Request from 'request-promise'
-
+import Request from "request-promise";
 
 //////////////////////////////////////////////COACHEE//////////////////////////////////////////////////////////////
 
-  export const alunoLogin = (AlunoID, AlunoPwd) => {
-        return new Promise((resolve, reject) =>{
-            Request({
-              method: 'GET',
-              url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=alunoLogin&AlunoID=${AlunoID}&AlunoPwd=${AlunoPwd}`,
-              json: true,
-              resolveWithFullResponse: true
-            }).then((response) => {
-              resolve (response.body);
-            }).catch((err) => {
-              console.log(err);
-              reject (err);
-            });
-      });
-  }
+export const alunoLogin = (AlunoID, AlunoPwd) => {
+  return { LOGIN: { ID_Aluno: 1, Status: 1 } };
+  // return new Promise((resolve, reject) => {
+  //   Request({
+  //     method: "GET",
+  //     url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=alunoLogin&AlunoID=${AlunoID}&AlunoPwd=${AlunoPwd}`,
+  //     json: true,
+  //     resolveWithFullResponse: true,
+  //   })
+  //     .then((response) => {
+  //       resolve(response.body);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       reject(err);
+  //     });
+  // });
+};
 
-  export const getTreinoSemana = () => {
-    return new Promise((resolve, reject) =>{
+export const getTreinoSemana = () => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoSemana`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body.TREINO_SEMANA);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+export const treinoHorarios = () => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoHorarios`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+export const treinoRegistra = (AlunoID, TipoTreinoID, HorarioID) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoRegistra&AlunoID=${AlunoID}&TipoTreinoID=${TipoTreinoID}&HorarioID=${HorarioID}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+export const treinoCheckOut = (TreinoID) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoCheckOut&TreinoID=${TreinoID}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+export const restricoesRegistra = (
+  AlunoID,
+  RestricoesObs,
+  RestricoesLocais
+) => {
+  if (RestricoesObs !== -1) {
+    return new Promise((resolve, reject) => {
       Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoSemana`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body.TREINO_SEMANA);
-        }).catch((err) => {
+        method: "GET",
+        url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${
+          process.env.REACT_APP_AUTH_TOKEN
+        }&Metodo=restricoesRegistra&AlunoID=${AlunoID}&RestricoesObs=${RestricoesObs}&RestricoesLocais=${RestricoesLocais.join()}`,
+        json: true,
+        resolveWithFullResponse: true,
+      })
+        .then((response) => {
+          resolve(response.body);
+        })
+        .catch((err) => {
           console.log(err);
-          reject (err);
-      });
-    });
-  }
-
-  export const treinoHorarios = () => {
-    return new Promise((resolve, reject) =>{
-      Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoHorarios`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-      });
-    });
-  }
-
-  export const treinoRegistra = (AlunoID, TipoTreinoID, HorarioID) => {
-    return new Promise((resolve, reject) =>{
-      Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoRegistra&AlunoID=${AlunoID}&TipoTreinoID=${TipoTreinoID}&HorarioID=${HorarioID}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-      });
-    });
-  }
-
-  export const treinoCheckOut = (TreinoID) => {
-    return new Promise((resolve, reject) =>{
-      Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoCheckOut&TreinoID=${TreinoID}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-      });
-    });
-  }
-
-
-
-  export const restricoesRegistra = (AlunoID, RestricoesObs, RestricoesLocais) => {
-    if (RestricoesObs!==-1) {
-      return new Promise((resolve, reject) =>{
-        Request({
-            method: 'GET',
-            url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=restricoesRegistra&AlunoID=${AlunoID}&RestricoesObs=${RestricoesObs}&RestricoesLocais=${RestricoesLocais.join()}`,
-            json: true,
-            resolveWithFullResponse: true
-          }).then((response) => {
-            resolve (response.body);
-          }).catch((err) => {
-            console.log(err);
-            reject (err);
+          reject(err);
         });
-      });
-    }
-    else {
-      return new Promise((resolve, reject) =>{
-        Request({
-            method: 'GET',
-            url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=restricoesRegistra&AlunoID=${AlunoID}&RestricoesLocais=${RestricoesLocais.join()}`,
-            json: true,
-            resolveWithFullResponse: true
-          }).then((response) => {
-            resolve (response.body);
-          }).catch((err) => {
-            console.log(err);
-            reject (err);
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      Request({
+        method: "GET",
+        url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${
+          process.env.REACT_APP_AUTH_TOKEN
+        }&Metodo=restricoesRegistra&AlunoID=${AlunoID}&RestricoesLocais=${RestricoesLocais.join()}`,
+        json: true,
+        resolveWithFullResponse: true,
+      })
+        .then((response) => {
+          resolve(response.body);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
         });
-      });
-    }
-  }
-
-  export const treinoAvalia = (TreinoID, Avaliacao) => {
-    return new Promise((resolve, reject) =>{
-      Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoAvalia&TreinoID=${TreinoID}&Avaliacao=${Avaliacao}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-      });
     });
   }
+};
 
-  export const resultadoRegistra = (AlunoID, DataResultado, Peso, Altura, AlongamentoNivel, Adbominais, Flexoes, Barra, Pescoco, Cintura, Quadril, Biceps, Coxa) => {
-    return new Promise((resolve, reject) =>{
-      Request({
-          method: 'GET',
-          url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=resultadoRegistra&AlunoID=${AlunoID}&DataResultado=${DataResultado}&Peso=${Peso}&Altura=${Altura}&AlongamentoNivel=${AlongamentoNivel}&Adbominais=${Adbominais}&Flexoes=${Flexoes}&Barra=${Barra}&Pescoco=${Pescoco}&Cintura=${Cintura}&Quadril=${Quadril}&Biceps=${Biceps}&Coxa=${Coxa}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
+export const treinoAvalia = (TreinoID, Avaliacao) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoAvalia&TreinoID=${TreinoID}&Avaliacao=${Avaliacao}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
       });
-    });
-  }
+  });
+};
 
+export const resultadoRegistra = (
+  AlunoID,
+  DataResultado,
+  Peso,
+  Altura,
+  AlongamentoNivel,
+  Adbominais,
+  Flexoes,
+  Barra,
+  Pescoco,
+  Cintura,
+  Quadril,
+  Biceps,
+  Coxa
+) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `${process.env.REACT_APP_API_URL}/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=resultadoRegistra&AlunoID=${AlunoID}&DataResultado=${DataResultado}&Peso=${Peso}&Altura=${Altura}&AlongamentoNivel=${AlongamentoNivel}&Adbominais=${Adbominais}&Flexoes=${Flexoes}&Barra=${Barra}&Pescoco=${Pescoco}&Cintura=${Cintura}&Quadril=${Quadril}&Biceps=${Biceps}&Coxa=${Coxa}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////COACH////////////////////////////////////////////////////////////////
 
-  export const getCoach = (matricula, pwd) => {
-    return new Promise((resolve, reject) =>{
-        Request({
-          method: 'GET',
-          url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=professorAutentica&ProfessorLogin=${matricula}&ProfessorPwd=${pwd}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-        });
-    });
-  }
+export const getCoach = (matricula, pwd) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=professorAutentica&ProfessorLogin=${matricula}&ProfessorPwd=${pwd}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
-  export const getCoachHorarios = (ProfessorID) => {
-    return new Promise((resolve, reject) =>{
-        Request({
-          method: 'GET',
-          url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=professorHoriarios&ProfessorID=${ProfessorID}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-        });
-    });
-  }
+export const getCoachHorarios = (ProfessorID) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=professorHoriarios&ProfessorID=${ProfessorID}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
-  export const treinoTransfere = (TreinoID, UsuarioDestinoID) => {
-    return new Promise((resolve, reject) =>{
-        Request({
-          method: 'GET',
-          url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoTransfere&TreinoID=${TreinoID}&UsuarioDestinoID=${UsuarioDestinoID}`,
-          json: true,
-          resolveWithFullResponse: true
-        }).then((response) => {
-          resolve (response.body);
-        }).catch((err) => {
-          console.log(err);
-          reject (err);
-        });
-    });
-  }
+export const treinoTransfere = (TreinoID, UsuarioDestinoID) => {
+  return new Promise((resolve, reject) => {
+    Request({
+      method: "GET",
+      url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.REACT_APP_AUTH_TOKEN}&Metodo=treinoTransfere&TreinoID=${TreinoID}&UsuarioDestinoID=${UsuarioDestinoID}`,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        resolve(response.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
