4e7d52550f897e4e49f7c35faa3f9c039fe99dd9
Diogo Almazan <diogodpa@outlook.com>
2021-05-31 17:12:46 -0300

    Ajustes jsconfig e eslintrc.json

--- src/pages/Coachee/CountdownTraining/Clock/Clock.js
+++ src/pages/Coachee/CountdownTraining/Clock/Clock.js
@@ -1,36 +1,30 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
-import moment from "moment";
-import "./Clock.css";
-import Timer from "react-compound-timer";
+import moment from 'moment'
+import './Clock.css'
+import Timer, { Hours, Minutes, Seconds } from 'react-compound-timer'
 
 export default function Clock({ startTime, onFinish }) {
-  const initialTime = moment(startTime, "h:mm:ss");
-  initialTime.subtract(moment().valueOf(), "millisecond");
-  return (
-    <Timer
-      initialTime={initialTime.valueOf()}
-      direction="backward"
-      checkpoints={[
-        {
-          time: 2000,
-          callback: () => {
-            const time = moment(startTime, "h:mm:ss");
-            time.subtract(moment().valueOf(), "millisecond");
-            if (time.valueOf() > 0) onFinish();
-          },
-        },
-      ]}
-    >
-      {() => (
-        <React.Fragment>
-          <div className="clockCountdownTraining">
-            <Timer.Hours />:
-            <Timer.Minutes />:
-            <Timer.Seconds />
-          </div>
-        </React.Fragment>
-      )}
-    </Timer>
-  );
+    const initialTime = moment(startTime, 'h:mm:ss')
+    initialTime.subtract(moment().valueOf(), 'millisecond')
+    return (
+        <Timer
+            initialTime={initialTime.valueOf()}
+            direction="backward"
+            checkpoints={[
+                {
+                    time: 2000,
+                    callback: () => {
+                        const time = moment(startTime, 'h:mm:ss')
+                        time.subtract(moment().valueOf(), 'millisecond')
+                        if (time.valueOf() > 0) onFinish()
+                    },
+                },
+            ]}
+        >
+            <div className="clockCountdownTraining">
+                <Hours />:
+                <Minutes />:
+                <Seconds />
+            </div>
+        </Timer>
+    )
 }
